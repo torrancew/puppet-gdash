6a360c9c8da14be71af75081a9ace83c6028711f
Tray Torrance <devwork@warrentorrance.com>
2013-04-18 21:59:51 -0700

    Fix typo in directory path

--- manifests/category.pp
+++ manifests/category.pp
@@ -2,7 +2,7 @@ define gdash::category() {
   Class['gdash::configure'] -> Gdash::Category[$title]
 
   file {
-    "${gdash::configure::template_dir}/${category}":
+    "${gdash::configure::template_dir}/${title}":
       ensure => directory,
       owner  => 'root',
       group  => 'root',
