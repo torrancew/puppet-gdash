e5ebae92e7cde8085f01504af8e1db8a6a670745
Tray Torrance <devwork@warrentorrance.com>
2013-04-18 22:13:54 -0700

    Update field handling

--- manifests/field.pp
+++ manifests/field.pp
@@ -1,5 +1,20 @@
-define gdash::field( $scale = 0.001, $color = 'red', $dashboard, $alias, $data ) {
+define gdash::field(
+  $scale = 1, $color = '', $category = 'servers',
+  $dashboard = $hostname, $alias = $title, $graph, $data
+) {
+  Gdash::Graph[$graph] -> Gdash::Field[$title]
+
   datacat_fragment {
+    target => "${gdash::configure::template_dir}/${category}/${dashboard}/${graph}.graph",
+    data   => {
+      fields => [{
+        name  => $title,
+        scale => $scale,
+        color => $color,
+        alias => $alias,
+        data  => $data,
+      }],
+    },
   }
 }
 
