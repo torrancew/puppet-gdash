fe85d88b065646dab651f3c53d6a9ce5b84f9a50
Tray Torrance <devwork@warrentorrance.com>
2013-04-18 22:04:29 -0700

    Fix path for graph files

--- manifests/graph.pp
+++ manifests/graph.pp
@@ -1,8 +1,8 @@
-define gdash::graph( $vtitle = '', $description = '', $dashboard ) {
+define gdash::graph( $vtitle = '', $description = '', $category = 'servers', $dashboard = $hostname ) {
   Gdash::Dashboard[$dashboard] -> Gdash::Graph[$title]
 
   datacat {
-    "${gdash::configure::template_dir}/${dashboard}/${title}.graph":
+    "${gdash::configure::template_dir}/${category}/${dashboard}/${title}.graph":
       template => 'gdash/graph.erb',
   }
 }
