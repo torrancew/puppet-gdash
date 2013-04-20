10c22a6e83ae0d7043d159e0067740588f3e316a
Tray Torrance <devwork@warrentorrance.com>
2013-04-19 21:50:53 -0700

    Fix dependency cycles

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -1,8 +1,3 @@
 class gdash::configure( $template_dir = '/etc/gdash/dashboards' ) {
-  Gdash::Category<||> -> Gdash::Category<<||>>
-
-  Gdash::Dashboard<<||>>
-  Gdash::Graph<<||>>
-  Gdash::Field<<||>>
 }
 
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,10 @@
 class gdash {
   anchor { 'gdash::begin': } -> class { 'gdash::configure': } -> anchor { 'gdash::end': }
+
+  Gdash::Category<||> -> Gdash::Category<<||>>
+
+  Gdash::Dashboard<<||>>
+  Gdash::Graph<<||>>
+  Gdash::Field<<||>>
 }
 
