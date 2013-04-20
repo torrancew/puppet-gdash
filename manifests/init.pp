9c870de3651d07c3f1e1ce18864ffdd8290126df
Tray Torrance <devwork@warrentorrance.com>
2013-04-19 23:06:11 -0700

    Fix the hellish cycle I created that completely locked up puppet

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,8 @@
 class gdash {
   anchor { 'gdash::begin': } -> class { 'gdash::configure': } -> anchor { 'gdash::end': }
 
-  Gdash::Category<||> -> Gdash::Category<<||>>
+  Gdash::Category<||>
+  Gdash::Category<<||>>
 
   Gdash::Dashboard<<||>>
   Gdash::Graph<<||>>
